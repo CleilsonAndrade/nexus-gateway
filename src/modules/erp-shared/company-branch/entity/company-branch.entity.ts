cd9c6bfbd2562146340b9ab7f7d02c8a7bc2fe30
CleilsonAndrade <cleilsonjose@hotmail.com>
2025-12-22 15:38:47 -0300

    feat: refactor employee and configuration entities; update imports and restructure files

---
+++ src/modules/erp-shared/company-branch/entity/company-branch.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity({
+  name: 'PCFILIAL',
+})
+export class CompanyBranchEntity {
+  @PrimaryColumn({
+    name: 'CODIGO',
+    type: 'varchar2',
+    nullable: false,
+    length: 2,
+  })
+  codeBranch: string;
+
+  @Column({
+    name: 'CGC',
+    type: 'varchar2',
+    length: 14,
+  })
+  cnpj: string;
+}
